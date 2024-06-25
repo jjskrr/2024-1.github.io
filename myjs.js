9cc96ec4e69e6104c3a0e7b38f32aaf60d3b7d65
jjskrr <167832765+jjskrr@users.noreply.github.com>
2024-06-25 10:41:30 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,12 @@
-alert('Hello,Osamu!);
+alert('Hello,Shota!);
+alert(1+1);
+alert('Hello,Shota!);
+alert(1+1);
+alert(Math.sprt(1+1));
+・２の平方根と３の平方根の和を計算しなさい。
+・１を０で割るとどうなるのか、試してみなさい。
+・０を０で割るとどうなるのか、試してください。
+alert(2+3)
+alert('2'+'3')
+alert(Number('2')+Number('3'))
+alert
